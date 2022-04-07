75f431ec9bb037b5de20db539b2988244480d067
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2022-04-07 20:03:08 +0200

    Install heroku cli

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { NextPage } from 'next'
 const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
+      <h1 className="flex items-center justify-center h-16 w-16 bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 md:w-20">Hello Next.js 👋</h1>
     </Layout>
   )
 }
